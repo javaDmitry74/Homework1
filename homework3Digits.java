390fc33c87178d802fbd421c3720dd4b3c46e474
javaDmitry74 <76561015+javaDmitry74@users.noreply.github.com>
2021-01-29 22:03:44 +0300

    Add files via upload

--- homework3Digits.java
+++ homework3Digits.java
@@ -1,33 +1,33 @@
 public class homework3Digits {
     public static void main(String[] args) {
-        float a = 129.67F;
-        float b = 135.23F;
-        float c = 119F;
+        float a = 190.67F;
+        float b = 188.23F;
+        float c = 189F;
         float average = (a + b + c) / 3F;
         System.out.println( "We have 3 numbers A , B, C : "+ a +", " + b + ", " + c );// printing out our numbers
         System.out.println("Average of all our numbers is = " + average);// print average of them
 
-        if (a > b && a > c && c < b ) { // we start process of finding min and max number
+        if (a > b && c < b ) { // we start process of finding min and max number
             System.out.println("A is highest number and it " + a);
             System.out.println(" and C is smallest number " + c);
             } else {
-            if (a > b && a > c && b < c) {
+            if (a > b &&  b < c) {
                 System.out.println("A is highest number and it " + a);
                 System.out.println(" and b is smallest number " + b);
                 } else {
-                if (b > a && b > c && c < a) {
+                if (b > a && c < a) {
                     System.out.println("B is highest number and it " + b);
                     System.out.println(" and C is smallest number " + c);
                     } else {
-                    if (b > c && b > a &&  a < c) {
+                    if (b > c && c < a) {
                         System.out.println("B is highest number and it " + b);
                         System.out.println(" and A is smallest number " + a);
                        } else {
-                       if (c > a && c > b && a < b ) {
+                       if (c > a &&  a < b ) {
                            System.out.println("C is highest number and it " + c);
                            System.out.println(" and A is smallest number " + a);
                            } else  {
-                           if (c > a && c > b && b < a ) {
+                           if (c > b && b < a ) {
                              System.out.println("C is highest number and it " + c);
                              System.out.println(" and B is smallest number " + b);
                              }
